524def45b975c9fa6a1b1098c2f2ca10b5cea34a
xsymmetry9 <glswim@gmail.com>
2024-10-30 16:43:42 +0800

    added questions and buttons

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Question = ({question}) =>{
-
+    
     return(
         <>
            <p className="text-center">{question}</p>
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,172 +1,99 @@
-import React, {useEffect, useState} from 'react';
-import axios from "axios";
-import {useContext} from "react";
 import { AppContext } from "../App";
-import AnswerChoices from "../components/AnswerChoices.jsx";
-import Message from "../components/Message.jsx";
-import Question from "../components/Question.jsx";
-import GameOver from "../components/GameOver.jsx";
-import { userLoader } from '../components/FetchUsers.jsx';
-import Loading from "../components/Loading.jsx";
-import HandleError from '../components/HandleError.jsx';
+import React, { useContext, useState } from "react";
+import Question from "../components/Question";
+import AnswerChoices from "../components/AnswerChoices";
+import Message from "../components/Message";
 
 const Game = () =>{
-    const [userId, setUserId] = useState("");
-    const [numberOfQuestionsAnswered, setNumberOfQuestionsAnswered] = useState(0);
-    const [userAnswer, setUserAnswer] = useState("");
-    const [questionIndex, setQuestionIndex] = useState(numberOfQuestionsAnswered);
-    const [isNextBtnHidden, setIsNextBtnHidden] = useState(true);
-    const [isSubmitBtnHidden, setIsSubmitBtnHidden] = useState(false);
-    const [message, setMessage] = useState("");
-    const [questionList, setQuestionList] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-
-    //Loads the questions via useContext
     const {questions} = useContext(AppContext);
 
-    //Function to reset the UI for the next question
-    const resetUI = () =>{
-        setUserAnswer("");
-    }
+    const [answeredQuestions, setAnsweredQuestions] = useState([]);
+    const [questionIndex, setQuestionIndex] = useState(0);
+    const [userAnswer, setUserAnswer] = useState("");
+    const [message, setMessage] = useState("");
+    const [isNextBtnHidden, setIsNextBtnHidden] = useState(true);
+    const [isSubmitBtnHidden, setIsSubmitBtnHidden] = useState(false);
+    const [numberCorrect, setNumberCorrect] = useState(0);
 
-    //Function to handler user's answer choice
     const userChoice = (e) =>{
         setUserAnswer(e.currentTarget.value);
     }
 
-    //Function to handle next button click
-    const nextBtnHandler = () =>{
-        const nextQuestion = generateIndex();
-        setQuestionIndex(nextQuestion);
-        setNumberOfQuestionsAnswered((prev) => prev + 1);
-        resetUI()
+    const nextQuestion = () =>{
+        setQuestionIndex((prev) => prev + 1);
+    }
+
+    const checkAnswer = () =>{
+        if(questions[questionIndex].answer === userAnswer)
+        {
+            setMessage(`You chose ${userAnswer}. You are correct`);
+            setNumberCorrect((prev) => prev + 1);
+
+        } else {
+            setMessage(`You chose ${userAnswer}. You are incorrect`);
+
+        }
+    }
+    const userSubmitHandler = () =>{
+        if(userAnswer === "")
+        {
+            setMessage("Answer the questions");
+        } else {
+            setAnsweredQuestions((prevList) => [...prevList, questions[questionIndex].id]);
+            setIsNextBtnHidden(false);
+            setIsSubmitBtnHidden(true);
+            checkAnswer();
+        }
+     
+    }
+
+    const userNextHandler = () =>{
+        setUserAnswer("");
+        setMessage("");
         setIsNextBtnHidden(true);
         setIsSubmitBtnHidden(false);
+        nextQuestion();
+    }
+
+    const restartHandler = () =>{
+        setAnsweredQuestions([]);
         setMessage("");
-
-        setQuestionList((prevList) => [...prevList, questions[nextQuestion].id]);
-
-    }
-    const generateIndex = () =>{
-        if(questions.length === 0) return 0;
-
-        let randomNumber;
-        do{
-            randomNumber = Math.floor(Math.random() * questions.length);
-        } while (questionList.includes(questions[randomNumber].id));
-
-        return randomNumber;
+        setUserAnswer("");
+        setIsNextBtnHidden(true);
+        setIsSubmitBtnHidden(false);
+        setQuestionIndex(0);
+        setNumberCorrect(0);
     }
 
-    useEffect(() =>{
-
-        // Function to fetch user and initialize the game
-        const fetchUsers = async () => {
-            try{
-                const response = await userLoader();
-                const {id, tests} = response.data[0];
-                if(response.data && response.data.length > 0 ){
-                    //Saves User's id
-                    setUserId(id); 
-
-                    //Calculates how many questions the user answered
-                    setNumberOfQuestionsAnswered(tests.length); 
-
-                    //Maps all the question_id into an array
-                    setQuestionList(tests.map((item) => item.question_id)); 
-
-                    //Generates a random index number
-                    const newRandomNumber = generateIndex();
-                    setQuestionIndex(newRandomNumber); 
-
-                    setLoading(false);
-
-                } else {
-                    console.log("No user found.");
-                    setError("No user found");
-                }
-            } catch (err){
-                setError("Failed to load data. Please try again later.");
-                setLoading(false);
-            }
-        }
-        fetchUsers();
-    }, [questions])
-
-    //Handle the submit of an answer
-    const handleSubmit = async (e) =>{
-        e.preventDefault();
-        // Let's user not move on to the next question until answering
-        if(userAnswer === "") { return; }
-
-        //Checks if the answer is correct
-        const isCorrect = questions[questionIndex].answer === userAnswer;
-
-        try{
-            //Stores necessary data 
-            const data = {
-                question_id: questions[questionIndex].id,
-                user_answer: userAnswer,
-                userId: userId,
-                isCorrect: isCorrect
-            }
-            //Sends it to the server side and uploads it to the db.
-            const response = await axios.post(`http://localhost:5000/api/users/answer`, data);
-
-            if(response.data.status){
-                
-                //Gets the answer of the question
-                const questionAnswer = questions[questionIndex].answer;
-                
-                isCorrect ? setMessage("You are correct!") : setMessage(`Sorry, the answer is ${questions[questionIndex].choices[questionAnswer]}`);
-                
-                //Hides the submit button and removes "hidden" from btn called "next"
-                setIsNextBtnHidden(false);
-                setIsSubmitBtnHidden(true);
-
-                return;
-            }
-        } catch (err){
-            //A simple log about the error
-            console.log("Error updating user: ", err);
-            setError("Error");
-        }
-    }
 
     return(
         <>
-            <div className="game-root">
-                {loading && <Loading />} 
-                {error && <HandleError message = {error}/>}
-                {!loading && <div className="game-container">
-                    <div className ="game-card">
-                        {numberOfQuestionsAnswered >= 10 ? (
-                            <>
-                                <GameOver userId = {userId} questions={questions}/>
-                            </> ) : (
-                                <>
-                                    <p className="text-center">Question {numberOfQuestionsAnswered + 1} / 10</p>
-                                    <Question question ={questions[questionIndex].question} />
-                                    <AnswerChoices triviaQuestion = {questions[questionIndex]} userAnswer = {userAnswer} userChoiceHandler = {userChoice}/>
-                                    <Message message = {message} />
-                                    <div id="button-navigations">
-                                        <form onSubmit={handleSubmit}>
-                                            <input type="hidden" name="answer" value={userAnswer} />
-                                            <input className={`btn-game submit ${isSubmitBtnHidden ? "hidden" : undefined}`} type="submit" value="Submit" />
-                                        </form>
-                                        <button className={`btn-game next ${isNextBtnHidden && 'hidden'}`} onClick={nextBtnHandler}>Next</button>
-                                    </div>
-                                </>
-                            ) 
-                        }
-                       
-                    </div>
-              
+        <div className="game-root">
+            <div className="game-container">
+                <div className="game-card">
+                    {answeredQuestions.length < 10 && (
+                        <>
+                            <p className="text-center">Question: {answeredQuestions.length + 1} / 10</p>
+                            <Question question = {questions[questionIndex].question} answeredQuestions = {answeredQuestions} />
+                            <AnswerChoices triviaQuestion = {questions[questionIndex]} userAnswer = {userAnswer} userChoiceHandler={userChoice} />
+                            <Message message={message}/>
+                            <div id="id-navigations">
+                                <button className = {`btn-game submit ${isSubmitBtnHidden ? "hidden" : undefined}`} onClick= {userSubmitHandler}>Submit</button>
+                                <button className = {`btn-game next ${isNextBtnHidden ? "hidden" : undefined}`} onClick = {userNextHandler}>Next</button>
+                            </div>
+                        </>
+                    )}
+                    {answeredQuestions.length >= 10 && (<>
+                        {/* {answeredQuestions.map((item) => <p>{item}</p>)} */}
+                        <p className="text-center">You have finished the test.</p>
+                        <p className="text-center">Here is your score.</p>
+                        <h1 className="text-center">{numberCorrect / 10 * 100}%</h1>
+                        <button className="btn-game restart" onClick = {restartHandler}>Restart</button>
+                    </>)}
+                   
                 </div>
-                }
             </div>
-  
+        </div>
         </>
     )
 }
